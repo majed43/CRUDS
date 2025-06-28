0967cec331fc3fb475354a5300a5b1f598b2856e
majed <majd121289@gmail>
2025-06-28 20:32:42 +0300

    make the showItem and upgrade ,delete featurs

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ let createButton=document.getElementById('createButton');
 let focusOn=document.getElementById('focus');
 let deviceInfo=document.getElementsByClassName('deviceInfo');
 let priceInputs=document.getElementsByClassName('price');
+let deleteAllButton=document.getElementById('deleteAll');
+let tbody=document.getElementById('tbody');
 // light on/off input function
 let lightOn=function(){
     return function(){
@@ -52,13 +54,13 @@ for (let i=0;i<priceInputs.length;i++){
 }
 // create item function
 let productsArray;
-if (localStorage.getItem('products')){
-    productsArray=JSON.parse(localStorage.getItem('products'))
-}else{
-    productsArray=[]
-}
 let createItem=function(){
     return function(){
+        if (localStorage.getItem('products')){
+            productsArray=JSON.parse(localStorage.getItem('products'))
+        }else{
+            productsArray=[]
+        }
         if (count.value){
             let details={
                 title:title.value,
@@ -67,14 +69,141 @@ let createItem=function(){
                 ads:ads.value,
                 discount:discount.value,
                 total:total.innerHTML,
-                count:count.value,
                 category:category.value,
+                count:count.value,
             }
-            console.log(details)
-            console.log(productsArray)
             productsArray.push(details);
             localStorage.setItem('products',JSON.stringify(productsArray));
         }
     }
 }
-createButton.addEventListener('click',createItem())
+// show items function
+let showItem=function(){
+    return function(){
+        tbody.innerHTML='';
+        if (localStorage.getItem('products')){
+            let products=JSON.parse(localStorage.getItem('products'));
+            for (let i=0;i<products.length;i++){
+                let product=products[i];
+                let key=Object.keys(product);
+                for (let j=0;j<product['count'];j++){
+                    let tr=document.createElement('tr');
+                    tbody.appendChild(tr);
+                    let td=document.createElement('td');
+                    tr.appendChild(td);
+                    td.innerText=i;
+                    for (let k=0;k<key.length-1;k++){
+                        let td=document.createElement('td');
+                        tr.appendChild(td);
+                        let productKey=key[k]
+                        td.innerHTML=product[productKey]
+                    }
+                    for (let o=0;o<3;o++){
+                        let td=document.createElement('td');
+                        tr.appendChild(td);
+                        if(o==0){
+                            let upgradeItemButton=document.createElement('button');
+                            td.appendChild(upgradeItemButton);
+                            upgradeItemButton.innerHTML='Upgrade';
+                            upgradeItemButton.classList.add('upgrade');
+                            upgradeItemButton.addEventListener('click',function(){
+                                this.style.backgroundColor='#50c878';
+                                this.style.letterSpacing='2px';
+                                }
+                            )
+                            upgradeItemButton.addEventListener('click',upgradeItem());
+                            upgradeItemButton.addEventListener('click',totalCount())
+                            upgradeItemButton.addEventListener('click',deleteItem());
+                            upgradeItemButton.addEventListener('click',showItem());
+
+
+                        }
+                        if (o==1){
+                            let deleteItemButton=document.createElement('button');
+                            td.appendChild(deleteItemButton);
+                            deleteItemButton.innerHTML='Delete';
+                            deleteItemButton.classList.add('delete');
+                            deleteItemButton.addEventListener('click',deleteItem());
+                            deleteItemButton.addEventListener('click',showItem());
+
+                        }
+                        if(o==2){
+                            let deleteIdButton=document.createElement('button');
+                            td.appendChild(deleteIdButton);
+                            deleteIdButton.innerHTML='DeleteId';
+                            deleteIdButton.classList.add('delete');
+                            deleteIdButton.addEventListener('click',deleteId());
+                            deleteIdButton.addEventListener('click',showItem())
+                        }
+                    }    
+                }
+            }
+        }
+    }
+}
+// clean inputs function
+let cleanInputs=function(){
+    return function(){
+        for (let i=0;i<deviceInfo.length;i++){
+            deviceInfo[i].value=''
+        }
+        total.innerHTML='';
+
+    }
+}
+// delete all items function
+let deleteAll=function(){
+    return function(){
+        localStorage.clear();
+        productsArray=[];
+    }
+}
+// delete Item function
+let deleteItem=function(){
+    return function(){
+        let parentNode=this.parentElement.parentElement;
+        let id=parentNode.getElementsByTagName('td')[0].innerHTML;
+        let products=JSON.parse(localStorage.getItem('products'));
+        let product=products[id];
+        product['count']--;
+        products[id]=product;
+        if(product['count']==0){
+            products.splice(id,1)
+        }
+        localStorage.setItem('products',JSON.stringify(products));
+    }
+}
+// delete id 
+let deleteId=function(){
+    return function(){
+        let parentNode=this.parentElement.parentElement;
+        let id=parentNode.getElementsByTagName('td')[0].innerHTML;
+        let products=JSON.parse(localStorage.getItem('products'));
+        products.splice(id,1);
+        localStorage.setItem('products',JSON.stringify(products));
+        productsArray=[]
+    }
+}
+//upgrade function
+let upgradeItem=function(){
+    return function(){
+        let parentNode=this.parentElement.parentElement;
+        let id=parentNode.getElementsByTagName('td')[0].innerHTML;
+        let products=JSON.parse(localStorage.getItem('products'));
+        let product=products[id];
+        let key=Object.keys(product);
+        key.splice(5,1);
+        for (let i=0;i<deviceInfo.length;i++){
+            let detail=key[i];
+            deviceInfo[i].value=product[detail];
+        }
+    }
+}
+// activation
+onload=showItem()
+createButton.addEventListener('click',createItem());
+createButton.addEventListener('click',showItem());
+createButton.addEventListener('click',cleanInputs());
+createButton.addEventListener('click',totalCount())
+deleteAllButton.addEventListener('click',deleteAll());
+deleteAllButton.addEventListener('click',showItem());
